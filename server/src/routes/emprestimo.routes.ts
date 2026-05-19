df91097f917c265b7f2beac11731574b71d9573f
Maysa <maysacampelo2@gmail.com>
2026-05-19 12:33:35 -0300

    feat: added emprestimo routes

--- server/src/routes/emprestimo.routes.ts
+++ server/src/routes/emprestimo.routes.ts
@@ -1,7 +1,12 @@
 import express from "express";
-
+import emprestimoController from "src/controllers/emprestimo.controller";
 const emprestimoRoutes = express.Router();
 
-// rotas de empréstimo aqui
+// empréstimo routes
+emprestimoRoutes.post('/', emprestimoController.create);
+emprestimoRoutes.get('/', emprestimoController.getAll);
+emprestimoRoutes.get('/busca', emprestimoController.getByClienteNome);
+emprestimoRoutes.delete('/:id', emprestimoController.delete);
+
 
 export default emprestimoRoutes;
