ba088c529978313ca8d782960939b12bf154e878
Safira Moraes <safira.moraes.dev@gmail.com>
2026-05-31 23:02:55 -0300

    refactor/completeBackendRefactor: Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,22 @@
-import routes from "./routes";
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import "@database";
-import { iniciarEnvioAutomaticoDeLembretes } from "./jobs/sendOverdueReminders.job";
-import { errorHandler } from "./middlewares/errorHandler";
+import routes from "./routes"
+import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+import "@database"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+export const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-app.use(errorHandler);
-app.use(express.static(__dirname + "/public"));
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.use(express.static(__dirname + "/public"))
 
-app.listen(process.env.SERVER_PORT || 3001, () => {
-  console.log("📦 Server running");
-});
-
-iniciarEnvioAutomaticoDeLembretes();
+if (require.main === module) {
+  const { iniciarEnvioAutomaticoDeLembretes } = require("src/jobs/enviarLembretesAtrasados.job")
+  app.listen(process.env.SERVER_PORT || 3001, () => {
+    console.log("📦 Server running")
+  })
+  iniciarEnvioAutomaticoDeLembretes()
+}
